5fc632e460da94713bddfe20788cbff30e559139
SantiagoP1405 <116893930+SantiagoP1405@users.noreply.github.com>
2025-03-25 21:19:16 -0600

    TRIGGER
    - Se introdujo el concepto de triggers en MySQL, explicando que son procedimientos especiales que se ejecutan automáticamente en respuesta a eventos como INSERT, UPDATE o DELETE en las tablas.
    - Se presentó un ejemplo práctico donde al insertar datos en la tabla de facturación, se actualiza automáticamente una tabla de logs con la acción realizada (Cliente Incluido o Cliente Excluido).
    - Se creó una tabla llamada tb_facturación con campos para FECHA y VENTA_TOTAL.
    - Se crearon dos tablas adicionales: tb_factura1 y items_facturas1, estableciendo las claves foráneas necesarias para mantener la integridad referencial.
    - Se realizaron inserciones de datos en las tablas de facturación y items de facturas, mostrando cómo se pueden relacionar los datos entre ellas.
    - Se explicó cómo realizar un INNER JOIN entre tb_factura1 y items_facturas1 para calcular la facturación total por fecha, utilizando la función SUM y agrupando los resultados con GROUP BY.
    - Se discutió la necesidad de automatizar el proceso de actualización de la facturación cada vez que se insertan nuevos datos, introduciendo la idea de implementar un trigger para este propósito.
    - Se dejó en claro que en el próximo video se mostrará cómo implementar el trigger para que la facturación se actualice automáticamente.

---
+++ trigger.sql
@@ -0,0 +1,76 @@
+CREATE TABLE tb_facturacion(
+	FECHA DATE NULL,
+    VENTA_TOTAL FLOAT 
+);
+
+SELECT * FROM tb_facturacion;
+
+CREATE TABLE `tb_factura1` (
+  `NUMERO` varchar(10) NOT NULL,
+  `FECHA` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `DNI` varchar(20) NOT NULL,
+  `MATRICULA` varchar(5) NOT NULL,
+  `IMPUESTO` float DEFAULT NULL,
+  PRIMARY KEY (`NUMERO`),
+  KEY `DNI1` (`DNI`),
+  KEY `MATRICULA1` (`MATRICULA`),
+  CONSTRAINT `tb_factura_ibfk_1_1` FOREIGN KEY (`DNI`) REFERENCES `tb_cliente` (`DNI`),
+  CONSTRAINT `tb_factura_ibfk_2_1` FOREIGN KEY (`MATRICULA`) REFERENCES `tb_vendedor` (`MATRICULA`)
+);
+
+CREATE TABLE `tb_items_facturas1` (
+  `NUMERO` varchar(5) NOT NULL,
+  `CODIGO` varchar(10) NOT NULL,
+  `CANTIDAD` int DEFAULT NULL,
+  `PRECIO` float DEFAULT NULL,
+  PRIMARY KEY (`NUMERO`,`CODIGO`),
+  KEY `CODIGO1` (`CODIGO`),
+  CONSTRAINT `tb_items_facturas_ibfk_1_1` FOREIGN KEY (`NUMERO`) REFERENCES `tb_factura` (`NUMERO`),
+  CONSTRAINT `tb_items_facturas_ibfk_2_1` FOREIGN KEY (`CODIGO`) REFERENCES `tb_producto` (`CODIGO`)
+);
+
+
+SELECT * FROM tb_items_facturas1;
+SELECT * FROM jugos_ventas.facturas;
+SELECT * FROM tb_factura1;
+SELECT * FROM tb_factura;
+SELECT * FROM tb_cliente;
+SELECT * FROM tb_vendedor;
+SELECT * FROM jugos_ventas.tabla_de_vendedores;
+SELECT * FROM tb_producto;
+
+INSERT INTO tb_factura1 VALUES(
+'0100',
+'2021-01-01',
+'1471156710',
+'235',
+0.10
+);
+
+INSERT INTO tb_items_facturas1 VALUES(
+'0100',
+'1002767',
+100,
+25
+),
+(
+'0100',
+'1004327',
+100,
+25
+),
+(
+'0100',
+'1013793',
+100,
+25
+);
+
+SELECT A.FECHA, SUM(B.CANTIDAD * B.PRECIO) AS VENTA_TOTAL
+FROM tb_factura1 A 
+INNER JOIN tb_items_facturas1 B 
+ON A.NUMERO = B.NUMERO
+GROUP BY A.FECHA;  
+
+
+
