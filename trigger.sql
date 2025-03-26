9c0476d78e6542a628f96648d75815911cb7e83f
SantiagoP1405 <116893930+SantiagoP1405@users.noreply.github.com>
2025-03-26 09:57:43 -0600

    TRIGGER V2
    -Se ha implementado un trigger en la base de datos para automatizar el proceso de actualización de la tabla de facturación cada vez que se inserten nuevos ítems en la tabla de items_facturas.
    - Se creó un delimitador personalizado utilizando `DELIMITER //` para permitir la definición del trigger sin que se interrumpa por los punto y coma.
    - Se definió el trigger llamado `TG_FACTURACION_INSERT`, que se activa `AFTER INSERT` en la tabla `tb_items_facturas`.
    - Dentro del trigger, se especificó que primero se debe ejecutar un comando `DELETE FROM tb_facturación` para limpiar la tabla de facturación antes de insertar los nuevos datos.
    - Luego, se realiza un `INSERT INTO tb_facturación` utilizando un `SELECT` que calcula la facturación total a partir de los ítems insertados.
    - Se verificó el funcionamiento del trigger insertando nuevos registros en `tb_items_facturas` y se observó que la tabla de facturación se actualiza automáticamente con el nuevo total.

--- trigger.sql
+++ trigger.sql
@@ -1,3 +1,4 @@
+USE ventas_jugos;
 CREATE TABLE tb_facturacion(
 	FECHA DATE NULL,
     VENTA_TOTAL FLOAT 
@@ -28,6 +29,9 @@ CREATE TABLE `tb_items_facturas1` (
   CONSTRAINT `tb_items_facturas_ibfk_1_1` FOREIGN KEY (`NUMERO`) REFERENCES `tb_factura` (`NUMERO`),
   CONSTRAINT `tb_items_facturas_ibfk_2_1` FOREIGN KEY (`CODIGO`) REFERENCES `tb_producto` (`CODIGO`)
 );
+
+SET foreign_key_checks = 0;
+SET SQL_SAFE_UPDATES = 0;
 
 
 SELECT * FROM tb_items_facturas1;
@@ -66,11 +70,109 @@ INSERT INTO tb_items_facturas1 VALUES(
 25
 );
 
+
 SELECT A.FECHA, SUM(B.CANTIDAD * B.PRECIO) AS VENTA_TOTAL
 FROM tb_factura1 A 
 INNER JOIN tb_items_facturas1 B 
 ON A.NUMERO = B.NUMERO
 GROUP BY A.FECHA;  
 
+DELIMITER // #Decirle al Trigger que se ejecutará todo lo que vaya después
+CREATE TRIGGER TG_FACTURACION_INSERT
+AFTER INSERT ON tb_items_facturas1
+FOR EACH ROW 
+BEGIN
+    DELETE FROM tb_facturacion;
+    
+    INSERT INTO tb_facturacion 
+    SELECT A.FECHA, SUM(B.CANTIDAD * B.PRECIO) AS VENTA_TOTAL
+    FROM tb_factura1 A 
+    INNER JOIN tb_items_facturas1 B 
+    ON A.NUMERO = B.NUMERO
+    GROUP BY A.FECHA;  
 
+END;
+//
 
+DELIMITER ;
+
+INSERT INTO tb_factura1 VALUES(
+'0102',
+'2021-01-01',
+'1471156710',
+'235',
+0.10
+);
+
+INSERT INTO tb_items_facturas1 VALUES(
+'0102',
+'1002767',
+100,
+25
+),
+(
+'0102',
+'1004327',
+100,
+25
+),
+(
+'0102',
+'1013793',
+100,
+25
+);
+
+INSERT INTO tb_factura1 VALUES(
+'0103',
+'2021-01-01',
+'1471156710',
+'235',
+0.10
+);
+
+INSERT INTO tb_items_facturas1 VALUES(
+'0103',
+'1002767',
+100,
+25
+),
+(
+'0103',
+'1004327',
+100,
+25
+),
+(
+'0103',
+'1013793',
+100,
+25
+);
+
+INSERT INTO tb_factura1 VALUES(
+'0104',
+'2021-01-01',
+'1471156710',
+'235',
+0.10
+);
+
+INSERT INTO tb_items_facturas1 VALUES(
+'0104',
+'1002767',
+200,
+25
+),
+(
+'0104',
+'1004327',
+200,
+25
+),
+(
+'0104',
+'1013793',
+200,
+25
+);
